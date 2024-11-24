8e4c6d75454d4f77042fa09d60970d7a2ae331d8
Richard Madison <rmadison@lightspeedtech.com>
2024-11-24 14:18:48 -0500

    Update installer to version 0.1.3

--- kisspanel-install.sh
+++ kisspanel-install.sh
@@ -5,7 +5,7 @@
 #----------------------------------------------------------#
 
 # Version: 0.1.0
-# Build Date: 2024-11-24 14:15:12
+# Build Date: 2024-11-24 14:18:48
 # Website: https://kisspanel.org
 # GitHub: https://github.com/kisspanel/kisspanel
 
